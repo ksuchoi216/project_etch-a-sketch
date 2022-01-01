18d08a3bc01e157c9ae7fbbd97298fec799977b3
KC <53473769+ksuchoi216@users.noreply.github.com>
2022-01-01 14:41:15 +0000

    update clear button

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 // variables
 const DEFAULT_SIZE_GRID = 16;
+const DEFAULT_COLOR_BACK = `white`;
+const DEFAULT_COLOR = `black`;
+let CURRENT_SIZE_GRID = 16;
 
 // selectors
-const grid = document.getElementById("grid")
+const grid = document.getElementById("grid");
+const sizeSlider = document.getElementById("sizeSlider");
+const sizeStatus = document.getElementById("sizeStatus");
+const clear = document.getElementById("clear");
 
+// simple method
+sizeSlider.onmousemove = (e) => {updateSizeStatus(e.target.value)};
+sizeSlider.onchange = (e) => {updateSizeGrid(e.target.value)};
+clear.onclick = (e) => {clearGrid(e)}; 
 
+function clearGrid(e) {
+  refreshGrid();
+  setupGrid(CURRENT_SIZE_GRID);
+}
 
+function updateSizeStatus(value) {
+  sizeStatus.innerHTML = `Size Selection: ${value} x ${value}`;
+};
 
+function updateSizeGrid(value) {
+  refreshGrid()
+  updateSizeStatus(value)
+  setupGrid(value)
+};
 
+function refreshGrid(){
+  grid.innerHTML = ``;
+  grid.style.backgroundColor = DEFAULT_COLOR_BACK;
+};
+
+function changeColor(e) {
+  e.target.style.backgroundColor = DEFAULT_COLOR;
+};
 
 // grid functions
 function setupGrid(size){
@@ -15,12 +45,13 @@ function setupGrid(size){
     // grid.innerHTML = `This is ${size}`;
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  
+    grid.style.backgroundColor = DEFAULT_COLOR_BACK;
+    CURRENT_SIZE_GRID = size;
+
     for (let i = 0; i < size * size; i++) {
       const gridElement = document.createElement(`div`);
       gridElement.classList.add(`eachGrid`);
-    //   gridElement.style.borderj='2px solid aqua';
-    //   gridElement.innerHTML = '*';
+      gridElement.addEventListener('mouseover', changeColor);
       grid.appendChild(gridElement);
     }
 
